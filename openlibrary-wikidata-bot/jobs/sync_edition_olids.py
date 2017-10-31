6d28cc70fc8f3d37c348a2830719d9f5983b6e64
Drini Cami <cdrini@gmail.com>
2017-10-31 01:25:23 -0400

    proper logging + dry_run option + handle multiple wd isbns better

--- openlibrary-wikidata-bot/jobs/sync_edition_olids.py
+++ openlibrary-wikidata-bot/jobs/sync_edition_olids.py
@@ -3,28 +3,33 @@ Finds editions on Wikidata and Open Library with the same ISBNs and adds the
 Open Library ID to Wikidata and the Wikidata ID to Open Library.
 """
 
+import datetime
 import logging
+from os import makedirs
 from typing import TypeVar, List, Callable, Hashable
 
+from olclient import OpenLibrary
 import pywikibot
 from pywikibot.data.sparql import SparqlQuery
-from olclient import OpenLibrary
 
 QUERY = """
 SELECT
 ?item
-#?itemLabel
-?isbn13
-?isbn10
+# ?itemLabel
+(group_concat(distinct ?isbn13; separator=";") as ?isbn13s)
+(group_concat(distinct ?isbn10; separator=";") as ?isbn10s)
 WHERE {
   ?item  wdt:P31 wd:Q3331189.                # instanceOf: Edition                 
   OPTIONAL { ?item wdt:P212 ?isbn13. }       # isbn13: ?isbn13
   OPTIONAL { ?item wdt:P957 ?isbn10. }       # isbn10: ?isbn10
   FILTER(bound(?isbn13) || bound(?isbn10))
   FILTER NOT EXISTS { ?item wdt:P648 ?olid } # Open Library ID: ?olid
-#  SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
+  # SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
 }
-LIMIT 1 # for testing
+GROUP BY ?item
+  # ?itemLabel
+# HAVING(count(?isbn13) > 1) # for testing
+# LIMIT 10 # for testing
 """
 
 
@@ -36,8 +41,8 @@ def make_str_claim(repo, prop: str, target: str) -> pywikibot.Claim:
 
 
 def normalize_isbn(isbn: str) -> str:
-    """Remove hyphens and make lowercase."""
-    return ''.join(isbn.split('-')).lower()
+    """Remove hyphens and make uppercase."""
+    return isbn.replace('-', '').upper()
 
 
 def remove_dupes(
@@ -55,31 +60,61 @@ def remove_dupes(
     return result
 
 
-def sync_edition_olids():
+# Setup logger
+logger = logging.getLogger("jobs.sync_edition_olids")
+logger.setLevel(logging.DEBUG)
+# The logging format
+log_formatter = logging.Formatter('%(name)s;%(levelname)-8s;%(asctime)s %(message)s')
+# Log warnings+ to console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.WARN)
+console_handler.setFormatter(log_formatter)
+logger.addHandler(console_handler)
+# Log INFO+ to the log file
+log_dir = 'logs/jobs/sync_edition_olids'
+makedirs(log_dir, exist_ok=True)
+log_file_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+log_file = log_dir + '/sync_edition_olids_%s.log' % log_file_datetime
+file_handler = logging.FileHandler(log_file)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
+
+
+def sync_edition_olids(dry_run=False):
     """
     Finds editions on Wikidata and Open Library with the same ISBNs and adds the
     Open Library ID to Wikidata and the Wikidata ID to Open Library.
     """
     wd = pywikibot.Site("wikidata", "wikidata")
     wd_repo = wd.data_repository()
-    wd_sparql = SparqlQuery()
+    wdqs = SparqlQuery()  # Wikidata Query Service
 
     ol = OpenLibrary()
 
-    wikidata_books = wd_sparql.select(QUERY)
-    logging.info("Found %d books to update on Wikidata", len(wikidata_books))
-    for row in wikidata_books:
+    wd_books = wdqs.select(QUERY)
+    logger.info("Found %d editions to update", len(wd_books))
+    ol_books_modified = 0
+    wd_items_modified = 0
+    for row in wd_books:
         qid = row['item'].split('/')[-1]
-        isbns = [row[key] for key in ['isbn10', 'isbn13'] if row[key]]
+        logger.debug("Processing %s", qid)
+
+        isbns = []
+        for isbnDDs in ['isbn13s', 'isbn10s']:
+            if not row[isbnDDs]:
+                continue
+            _isbns = row[isbnDDs].split(';')
+            if len(_isbns) > 1:
+                logger.warning("%s has multiple %s (%d)", qid, isbnDDs, len(_isbns))
+            isbns += _isbns
         ol_books = [ol.Edition.get(isbn=normalize_isbn(isbn)) for isbn in isbns]
         ol_books = [book for book in ol_books if book]
         ol_books = remove_dupes(ol_books, lambda ed: ed.olid)
 
-        if len(ol_books) == 0:
-            logging.warning("No Open Library books for %s (isbns %s)", qid, ', '.join(isbns))
-            continue
+        logger.info("Found %d Open Library book(s) for %s (isbns %s)", len(ol_books), qid, ', '.join(isbns))
         if len(ol_books) > 1:
-            logging.warning("Multiple (%d) Open Library books for %s (isbns %s)", len(ol_books), qid, ', '.join(isbns))
+            logger.warning("Multiple (%d) Open Library books for %s (isbns %s)", len(ol_books), qid, ', '.join(isbns))
 
         # update open library data
         for book in ol_books:
@@ -89,24 +124,32 @@ def sync_edition_olids():
             book_qids = book.identifiers['wikidata']
 
             if qid in book_qids:
-                logging.warning("%s already has qid %s", book.olid, qid)
+                logger.warning("%s already has qid %s", book.olid, qid)
                 continue
 
             book_qids.append(qid)
             if len(book_qids) > 1:
-                logging.warning("%s now has multiple (%d) qids (%s)", book.olid, len(book_qids), ', '.join(book_qids))
-            book.save("[sync_edition_olids] add wikidata identifier")
+                logger.warning("%s now has multiple (%d) qids (%s)", book.olid, len(book_qids), ', '.join(book_qids))
+            if not dry_run:
+                ol.save("[sync_edition_olids] add wikidata identifier")
+            logger.debug("Added %s to %s", qid, book.olid)
+            ol_books_modified += 1
 
         # update wikidata data
         for book in ol_books:
             item = pywikibot.ItemPage(wd_repo, qid)
             claim = make_str_claim(wd_repo, 'P648', book.olid)
-            item.addClaim(claim)
+            if not dry_run:
+                item.addClaim(claim)
+            logger.debug("Added %s to %s", book.olid, qid)
+            wd_items_modified += 1
+    logger.info("Updated %d Open Library books and %d Wikidata items", ol_books_modified, wd_items_modified)
 
 
 if __name__ == "__main__":
+    console_handler.setLevel(logging.INFO)
     try:
-        sync_edition_olids()
+        sync_edition_olids(dry_run=True)
     except Exception as e:
-        logging.exception("")
+        logger.exception("")
         raise e
